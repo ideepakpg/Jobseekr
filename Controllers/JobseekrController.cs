197a7a48c255a6213245f208006aed4f767c7678
ideepakpg <deepakpg121@gmail.com>
2023-10-25 20:43:12 +0530

    Create JobseekrController and add stub actions

---
+++ Controllers/JobseekrController.cs
@@ -0,0 +1,37 @@
+﻿using Jobseekr.Models;
+using System.Web.Mvc;
+
+namespace Jobseekr.Controllers
+{
+    public class JobseekrController : Controller
+    {
+        EmployeeDBContext obj = new EmployeeDBContext();
+        // GET: Jobseekr
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action to handle landing page
+        public ActionResult LandingPage()
+        {
+            // Add your logic here
+            return View();
+        }
+
+        // Action to handle user login
+        public ActionResult Login()
+        {
+            // Add your login logic here
+            return View();
+        }
+
+        // Action to handle user registration
+        public ActionResult Register()
+        {
+            // Add your registration logic here
+            return View();
+        }
+
+    }
+}
