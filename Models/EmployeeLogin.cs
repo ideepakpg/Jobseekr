8b07263459d29fcf0471e0712b18df6717516dd7
ideepakpg <deepakpg121@gmail.com>
2023-10-25 20:33:10 +0530

    Create EmployeeLogin model class

---
+++ Models/EmployeeLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("EmployeeLogin")]
+    public class EmployeeLogin
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
