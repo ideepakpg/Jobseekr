f9e2496cdbcf420bb883ba7fffec44d5f14f07b1
ideepakpg <deepakpg121@gmail.com>
2023-10-26 21:30:35 +0530

    Rename context class to JobseekrDBContext

---
+++ Models/JobseekrDBContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Jobseekr.Models
+{
+    public class JobseekrDBContext : DbContext
+    {
+        public DbSet<EmployeeLogin> employeeLogins { get; set; }
+    }
+}
