6fb1eb1fe060b8a67d52f91963a16849a41be0bb
ideepakpg <deepakpg121@gmail.com>
2023-10-26 21:31:57 +0530

    Delete EmployeeDBContext.cs

--- Models/EmployeeDBContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace Jobseekr.Models
-{
-    public class EmployeeDBContext : DbContext
-    {
-        public DbSet<EmployeeLogin> employeeLogins { get; set; }
-    }
-}
