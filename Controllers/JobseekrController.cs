f929f7660c1752d2e526e4578f61b99b5447e632
ideepakpg <deepakpg121@gmail.com>
2023-10-27 16:15:48 +0530

    Refactor JobseekrController for improved functionality

--- Controllers/JobseekrController.cs
+++ Controllers/JobseekrController.cs
@@ -1,7 +1,6 @@
 ﻿using Jobseekr.Models;
 using System.Linq;
 using System.Web.Mvc;
-using System.Web.Security;
 
 namespace Jobseekr.Controllers
 {
@@ -15,10 +14,20 @@ namespace Jobseekr.Controllers
         }
 
         // Action to handle landing page
-        public ActionResult LandingPage()
+        public ActionResult LandingPage(string viewName = "LandingPage")
         {
-            // Add your logic here
-            return View();
+
+            if (viewName == "Login")
+            {
+                // to redirect to Login page
+                return View("Login");
+            }
+            else if (viewName == "Registration")
+            {
+                // to redirect to Registration page
+                return View("Registration");
+            }
+            return View("LandingPage");
         }
 
         // Action to handle user login
@@ -26,27 +35,85 @@ namespace Jobseekr.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = obj.employeeLogins.SingleOrDefault(u => u.Username == model.Username);
+                using (var dbContext = new JobseekrDBContext())
+                {
+                    var user = obj.employeeLogins.SingleOrDefault(u => u.Username == model.Username);
 
-                if (user != null && user.Password == model.Password)
-                {
-                    FormsAuthentication.SetAuthCookie(model.Username, false);
-                    return RedirectToAction("Welcome"); // Redirect to a secure page
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Invalid username or password.");
+                    if (user != null && user.Password == model.Password)
+                    {
+
+                        //FormsAuthentication.SetAuthCookie(model.Username, false);
+
+                        return RedirectToAction("WelcomePage");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid username or password.");
+                    }
                 }
             }
             return View(model);
         }
 
-        // Action to handle user registration
-        public ActionResult Register()
+        public ActionResult WelcomePage()
         {
-            // Add your registration logic here
+            var jobListings = obj.jobListings.ToList();
+            return View(jobListings);
+        }
+
+
+        // Action to handle user registration
+        [HttpGet]
+        public ActionResult Registration()
+        {
+
+            EmployeeRegistration obj2 = new EmployeeRegistration();
+            obj.employeeRegistrations.Add(obj2);
+            // If registration data is not valid, redisplay the registration form with validation errors
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registration(EmployeeRegistration employeeRegistrations)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var dbContext = new JobseekrDBContext())
+                {
+                    // Create instances for both models (to insert in both tables)
+                    var registration = new EmployeeRegistration
+                    {
+                        FirstName = employeeRegistrations.FirstName,
+                        LastName = employeeRegistrations.LastName,
+                        EmailId = employeeRegistrations.EmailId,
+                        Username = employeeRegistrations.Username,
+                        Password = employeeRegistrations.Password,
+                        MobileNumber = employeeRegistrations.MobileNumber
+                    };
+
+                    var login = new EmployeeLogin
+                    {
+                        Username = employeeRegistrations.Username,
+                        Password = employeeRegistrations.Password
+                    };
+
+                    // Add and save the instances to their respective tables
+                    dbContext.employeeRegistrations.Add(registration);
+                    dbContext.employeeLogins.Add(login);
+                    dbContext.SaveChanges();
+                }
+
+                return RedirectToAction("RegistrationSuccess");
+            }
+
+            return View();
+        }
+
+
+        public ActionResult RegistrationSuccess()
+        {
+            // logic here
+            return View();
+        }
     }
 }
