aa8cfc7bafc406efcc620e69d8709ccaeb749bda
ideepakpg <deepakpg121@gmail.com>
2023-10-27 16:12:13 +0530

    Create class for employee registration

---
+++ Models/EmployeeRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("Registration")]
+    public class EmployeeRegistration
+    {
+        [Key]
+        public int RegId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
