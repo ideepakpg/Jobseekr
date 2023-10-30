482d59f5b4587a9f69ad2e85306fb26d8506a313
ideepakpg <deepakpg121@gmail.com>
2023-10-30 20:58:27 +0530

    Add CompanyProfile DbSet to JobseekrDBContext

--- Models/JobseekrDBContext.cs
+++ Models/JobseekrDBContext.cs
@@ -7,5 +7,6 @@ namespace Jobseekr.Models
         public DbSet<EmployeeLogin> employeeLogins { get; set; }
         public DbSet<EmployeeRegistration> employeeRegistrations { get; set; }
         public DbSet<JobListing> jobListings { get; set; }
+        public DbSet<CompanyProfile> companyProfiles { get; set; }
     }
 }
