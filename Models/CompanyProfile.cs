3884ed06b96b1f58f384f377c2a09cca14664994
ideepakpg <deepakpg121@gmail.com>
2023-10-30 20:57:22 +0530

    Create CompanyProfile model class

---
+++ Models/CompanyProfile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("CompanyProfile")]
+    public class CompanyProfile
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int Founded { get; set; }
+        public long Contact { get; set; }
+        public string Website { get; set; }
+    }
+}
