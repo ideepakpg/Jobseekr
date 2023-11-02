bdcaa8efa8f4cf3590a3a687201c21af3120fe4e
ideepakpg <deepakpg121@gmail.com>
2023-11-02 16:32:44 +0530

    Add JobApplication model class

---
+++ Models/JobApplication.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("JobApplication")]
+    public class JobApplication
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ApplicationId { get; set; }
+        [ForeignKey("JobListing")]
+        public int JobId { get; set; }
+        public string ApplicantName { get; set; }
+        public string ApplicantEmail { get; set; }
+        public string Resume { get; set; }
+        public JobListing JobListing { get; set; }
+    }
+}
