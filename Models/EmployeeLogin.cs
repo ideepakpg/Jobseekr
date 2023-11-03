69828257e212244006506ee2b33cdf7480a6957b
ideepakpg <deepakpg121@gmail.com>
2023-11-03 20:22:35 +0530

    Enhance EmployeeLogin model for Role-Based Authentication

--- Models/EmployeeLogin.cs
+++ Models/EmployeeLogin.cs
@@ -4,11 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Jobseekr.Models
 {
     [Table("Login")]
-    public class EmployeeLogin
+    public class EmployeeLogin     //this model class for both employee and employer to login
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
