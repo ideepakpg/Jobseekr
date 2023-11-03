007941da7cd75e1797d99dbd7eb28098b59ed6da
ideepakpg <deepakpg121@gmail.com>
2023-11-03 20:19:25 +0530

    Create EmployerRegistration model class

---
+++ Models/EmployerRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("EmployerRegistration")]
+    public class EmployerRegistration
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Mobile Number is required")]
+        public string MobileNumber { get; set; }
+    }
+}
