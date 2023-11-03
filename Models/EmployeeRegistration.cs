1451d33679d253ab0ecb64ebea89320ebb7ab319
ideepakpg <deepakpg121@gmail.com>
2023-11-03 20:20:53 +0530

    Update database table and implement validation

--- Models/EmployeeRegistration.cs
+++ Models/EmployeeRegistration.cs
@@ -3,16 +3,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Jobseekr.Models
 {
-    [Table("Registration")]
+    [Table("EmployeeRegistration")]
     public class EmployeeRegistration
     {
         [Key]
         public int RegId { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Second Name is required")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Email Id is required")]
         public string EmailId { get; set; }
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Mobile Number is required")]
         public string MobileNumber { get; set; }
     }
 }
