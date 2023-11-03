89d5ddfa6af4e978ccd8a4062e7520da735edf2c
ideepakpg <deepakpg121@gmail.com>
2023-11-03 20:17:45 +0530

    Implement validation in JobApplication model

--- Models/JobApplication.cs
+++ Models/JobApplication.cs
@@ -11,8 +11,11 @@ namespace Jobseekr.Models
         public int ApplicationId { get; set; }
         [ForeignKey("JobListing")]
         public int JobId { get; set; }
+        [Required(ErrorMessage = "Applicant name is required")]
         public string ApplicantName { get; set; }
+        [Required(ErrorMessage = "Applicant Email is required")]
         public string ApplicantEmail { get; set; }
+        [Required(ErrorMessage = "Resume link is required")]
         public string Resume { get; set; }
         public JobListing JobListing { get; set; }
     }
