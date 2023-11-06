d517f6b3a4ca38fa809dbacd04fed6bc21a18bec
ideepakpg <deepakpg121@gmail.com>
2023-11-06 21:26:20 +0530

    Refactor DbContext for Login and EmployeeLogin model

--- Models/JobseekrDBContext.cs
+++ Models/JobseekrDBContext.cs
@@ -4,12 +4,13 @@ namespace Jobseekr.Models
 {
     public class JobseekrDBContext : DbContext
     {
-        public DbSet<EmployeeLogin> employeeLogins { get; set; }
+        public DbSet<Login> logins { get; set; }
         public DbSet<EmployeeRegistration> employeeRegistrations { get; set; }
         public DbSet<JobListing> jobListings { get; set; }
         public DbSet<CompanyProfile> companyProfiles { get; set; }
         public DbSet<Enquiry> enquiryListings { get; set; }
         public DbSet<JobApplication> jobApplicationListings { get; set; }
         public DbSet<EmployerRegistration> employerRegistrations { get; set; }
+        public DbSet<EmployeeLogin> employeeLoginsforProfile { get; set; }
     }
 }
