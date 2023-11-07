b3abbc404bcaa1caa58b4227a182a8457a50d3e4
ideepakpg <deepakpg121@gmail.com>
2023-11-07 19:49:26 +0530

    Create model for storing employer login details

---
+++ Models/LoginEmployer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("LoginEmployer")]
+    public class LoginEmployer      //this model class is for only employers to login
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
