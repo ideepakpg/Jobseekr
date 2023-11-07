f665e28334bb14fab0a2943099ad4468b5419fc8
ideepakpg <deepakpg121@gmail.com>
2023-11-07 20:04:46 +0530

    Create EmployerLogin model for fetching employer details

---
+++ Models/EmployerLogin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("EmployerLoginProfile")]
+
+    public class EmployerLogin      // this model is for to fetch and pass employer details to EmployerProfile view(My Profile)
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+        public string Role { get; set; }
+    }
+}
