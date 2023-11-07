173a1d335f68f61fa375b6e41fa83fb2fccdd6b7
ideepakpg <deepakpg121@gmail.com>
2023-11-07 19:57:15 +0530

    Create model for both employee and employer to login

---
+++ Models/LoginCommon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jobseekr.Models
+{
+    public class LoginCommon    // to login for both employee and employer (for Login view model to change model from Jobseekr.Models.Login to Jobseekr.Models.LoginCommon)
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
