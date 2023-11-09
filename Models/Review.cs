1ff04f4563030514dfa58c4cca3dc6594f56a45b
ideepakpg <deepakpg121@gmail.com>
2023-11-09 20:45:45 +0530

    Create Review model class

---
+++ Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jobseekr.Models
+{
+    [Table("ReviewsandRatings")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public int JobId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public string Id { get; set; }
+    }
+}
